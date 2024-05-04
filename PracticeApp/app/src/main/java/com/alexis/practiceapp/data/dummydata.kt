4f0771bd17214a5b6722c5f07cf1787a4a83707d
Alexis00002521 <00002521@uca.edu.sv>
2024-05-03 18:25:26 -0600

    delete to another

--- PracticeApp/app/src/main/java/com/alexis/practiceapp/data/dummydata.kt
+++
@@ -1,9 +0,0 @@
-package com.alexis.practiceapp.data
-
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import com.alexis.practiceapp.model.objectSong
-
-
-val myplaylist : MutableState<MutableList<objectSong>> = mutableStateOf(mutableListOf())
-// play list - es un estado mutable que es una lista de objeto cancion que es una lista de canciones que posee objeto Song
